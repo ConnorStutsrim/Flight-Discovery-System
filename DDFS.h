a2d3c8b6c902c627af8e6aae050201103af2b1c3
rvalent4 <rvalent4@binghamton.edu>
2014-12-05 19:12:54 -0500

    Update DDFS.h

--- DDFS.h
+++ DDFS.h
@@ -1,18 +1,32 @@
 #ifndef DDFS_H_
 #define DDFS_H_
-
 #include "DepartNode.h"
 #include <vector>
 
+struct data{
+string currentCity;
+int cumulativeTime;
+string previousCity;
+};
+
 class DDFS {
 private:
+	std::vector<data> dataVector;
 	std::vector<DepartNode> departureNodes;
+	std::vector<Flight> flights;
+	string startingCity;
+	string destinationCity;
+	bool firstTravelIteration;
 	
 public:
 	DDFS();
-
+	void JustGetMeThereToday(string start,string prevCity ,string dest, int currentTime);
+	void setFlightVector();
 	void addFlight(Flight);
+	void createDataVector();
 	void print();
+	void printDataVector();
+
 };
 
 #endif
